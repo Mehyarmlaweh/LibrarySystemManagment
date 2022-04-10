9c5bca6322c9ed0f90d734fdcfba46ccd32e8508
Mehyarmlaweh <86748310+Mehyarmlaweh@users.noreply.github.com>
2022-04-10 18:14:09 +0100

    database add

---
+++ DATABASE/lms.sql
@@ -0,0 +1,177 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : dim. 10 avr. 2022 à 17:08
+-- Version du serveur : 5.7.36
+-- Version de PHP : 7.4.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `lms`
+--
+
+DELIMITER $$
+--
+-- Procédures
+--
+DROP PROCEDURE IF EXISTS `generate_due_list`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `generate_due_list` ()  NO SQL
+SELECT I.issue_id, M.email, B.isbn, B.title
+FROM book_issue_log I INNER JOIN member M on I.member = M.username INNER JOIN book B ON I.book_isbn = B.isbn
+WHERE DATEDIFF(CURRENT_DATE, I.due_date) >= 0 AND DATEDIFF(CURRENT_DATE, I.due_date) % 5 = 0 AND (I.last_reminded IS NULL OR DATEDIFF(I.last_reminded, CURRENT_DATE) <> 0)$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `book`
+--
+
+DROP TABLE IF EXISTS `book`;
+CREATE TABLE IF NOT EXISTS `book` (
+  `isbn` char(13) NOT NULL,
+  `title` varchar(80) NOT NULL,
+  `author` varchar(80) NOT NULL,
+  `category` varchar(80) NOT NULL,
+  `price` int(4) UNSIGNED NOT NULL,
+  `copies` int(10) UNSIGNED NOT NULL,
+  PRIMARY KEY (`isbn`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `book`
+--
+
+INSERT INTO `book` (`isbn`, `title`, `author`, `category`, `price`, `copies`) VALUES
+('1010101', 'Test Book', 'Tester', 'Medical', 30, 25),
+('6900152484440', 'Data Science', 'tom fawcett', 'Tech', 299, 0),
+('9780856680304', 'Studies in Modern Arabic Literature', 'Robin C. Ostle', 'Literature', 40, 13),
+('9782616052277', 'X-Men: God Loves, Man Kills', 'Chris', 'Comics', 399, 33),
+('9783161484100', 'Mike Tyson : Undisputed Truth', 'Larry Sloman, Mike Tyson', 'Sports', 299, 18);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `book_issue_log`
+--
+
+DROP TABLE IF EXISTS `book_issue_log`;
+CREATE TABLE IF NOT EXISTS `book_issue_log` (
+  `issue_id` int(11) NOT NULL AUTO_INCREMENT,
+  `member` varchar(20) NOT NULL,
+  `book_isbn` varchar(13) NOT NULL,
+  `due_date` date NOT NULL,
+  `last_reminded` date DEFAULT NULL,
+  PRIMARY KEY (`issue_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `book_issue_log`
+--
+
+INSERT INTO `book_issue_log` (`issue_id`, `member`, `book_isbn`, `due_date`, `last_reminded`) VALUES
+(1, 't', '9783161484100', '2022-04-09', '2022-04-09');
+
+--
+-- Déclencheurs `book_issue_log`
+--
+DROP TRIGGER IF EXISTS `issue_book`;
+DELIMITER $$
+CREATE TRIGGER `issue_book` BEFORE INSERT ON `book_issue_log` FOR EACH ROW BEGIN
+	SET NEW.due_date = DATE_ADD(CURRENT_DATE, INTERVAL 7 DAY);
+    UPDATE member SET balance = balance - (SELECT price FROM book WHERE isbn = NEW.book_isbn) WHERE username = NEW.member;
+    UPDATE book SET copies = copies - 1 WHERE isbn = NEW.book_isbn;
+    DELETE FROM pending_book_requests WHERE member = NEW.member AND book_isbn = NEW.book_isbn;
+END
+$$
+DELIMITER ;
+DROP TRIGGER IF EXISTS `return_book`;
+DELIMITER $$
+CREATE TRIGGER `return_book` BEFORE DELETE ON `book_issue_log` FOR EACH ROW BEGIN
+    UPDATE member SET balance = balance + (SELECT price FROM book WHERE isbn = OLD.book_isbn) WHERE username = OLD.member;
+    UPDATE book SET copies = copies + 1 WHERE isbn = OLD.book_isbn;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `librarian`
+--
+
+DROP TABLE IF EXISTS `librarian`;
+CREATE TABLE IF NOT EXISTS `librarian` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) NOT NULL,
+  `password` char(40) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `librarian`
+--
+
+INSERT INTO `librarian` (`id`, `username`, `password`) VALUES
+(1, 'mehyar', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `member`
+--
+
+DROP TABLE IF EXISTS `member`;
+CREATE TABLE IF NOT EXISTS `member` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) NOT NULL,
+  `password` char(40) NOT NULL,
+  `name` varchar(80) NOT NULL,
+  `email` varchar(80) NOT NULL,
+  `balance` int(4) NOT NULL,
+  `state` varchar(20) NOT NULL DEFAULT 'pending',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `member`
+--
+
+INSERT INTO `member` (`id`, `username`, `password`, `name`, `email`, `balance`, `state`) VALUES
+(7, 't', '8efd86fb78a56a5145ed7739dcb00c78581c5375', 't', 't@gmail.com', 552, 'active'),
+(8, 's', 'a0f1490a20d0211c997b44bc357e1972deab8ae3', 's', 's@gmail.co', 1200, 'active');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `pending_book_requests`
+--
+
+DROP TABLE IF EXISTS `pending_book_requests`;
+CREATE TABLE IF NOT EXISTS `pending_book_requests` (
+  `request_id` int(11) NOT NULL AUTO_INCREMENT,
+  `member` varchar(20) NOT NULL,
+  `book_isbn` varchar(13) NOT NULL,
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`request_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
